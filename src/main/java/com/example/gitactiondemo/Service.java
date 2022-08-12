4cdb4431a699669cbaded6bb5ceed473cd3f7f57
Sihang Li <li54718579@gmail.com>
2022-08-12 09:23:54 -0400

    something

---
+++ src/main/java/com/example/gitactiondemo/Service.java
@@ -0,0 +1,8 @@
+package com.example.gitactiondemo;
+
+@org.springframework.stereotype.Service
+public class Service {
+    public void someService(){
+        System.out.println("this is a service method");
+    }
+}
